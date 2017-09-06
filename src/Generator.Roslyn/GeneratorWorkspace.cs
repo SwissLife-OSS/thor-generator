e62828d3915be365e458cd6599a7867d68dc501f
Michael Staib <michael@chillicream.com>
2017-09-06 22:31:07 +0200

    Startet implementing workspace that can handle msbuild projects and the newer msbuild base core projects

---
+++ src/Generator.Roslyn/GeneratorWorkspace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host;
+
+namespace ChilliCream.Tracing.Generator.Roslyn
+{
+    public class GeneratorWorkspace
+        : Workspace
+    {
+        internal GeneratorWorkspace(HostServices host)
+            : base(host, nameof(GeneratorWorkspace))
+        {
+        }
+    }
+}
