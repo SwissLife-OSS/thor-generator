acd0eccc8cd10ffc479d96c3c74ccbb4e720f3af
Michael Staib <michael@chillicream.com>
2017-09-15 15:30:57 +0200

    added document tests

--- src/Generator.ProjectSystem.Tests/DocumentIdTests.cs
+++ src/Generator.ProjectSystem.Tests/DocumentIdTests.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using System.Linq;
+using ChilliCream.Logging.Generator;
+using FluentAssertions;
 using Xunit;
 
 namespace Generator.ProjectSystem.Tests
@@ -8,7 +12,93 @@ namespace Generator.ProjectSystem.Tests
         [Fact]
         public void DocumentIdToString()
         {
+            // arrange
+            DocumentId documentId = new DocumentId("foo.cs", new string[] { "a", "b" });
 
+            // act
+            string result = documentId.ToString();
+
+            // assert
+            string expectedString = string.Concat("a", Path.DirectorySeparatorChar,
+                "b", Path.DirectorySeparatorChar, "foo.cs");
+            result.Should().Be(expectedString);
+        }
+
+
+        [InlineData("a,b,f.cs", "A,b,f.cs", false)]
+        [InlineData("a,b,f.cs", "c,d,f.cs", false)]
+        [InlineData("a,b,f.cs", "a,b,f.cs", true)]
+        [Theory]
+        public void DocumentIdEquality(string a, string b, bool expectedResult)
+        {
+            // arrange
+            DocumentId ida = DocumentIdFromString(a, ',');
+            DocumentId idb = DocumentIdFromString(b, ',');
+
+            // act
+            bool result = ida.Equals(idb);
+
+            // assert
+            result.Equals(expectedResult);
+        }
+
+        [InlineData("a,b,f.cs", "A,b,f.cs", false)]
+        [InlineData("a,b,f.cs", "c,d,f.cs", false)]
+        [InlineData("a,b,f.cs", "a,b,f.cs", true)]
+        [Theory]
+        public void DocumentIdEqualsOperator(string a, string b, bool expectedResult)
+        {
+            // arrange
+            DocumentId ida = DocumentIdFromString(a, ',');
+            DocumentId idb = DocumentIdFromString(b, ',');
+
+            // act
+            bool result = ida == idb;
+
+            // assert
+            result.Equals(expectedResult);
+        }
+
+        [InlineData("a,b,f.cs", "A,b,f.cs", true)]
+        [InlineData("a,b,f.cs", "c,d,f.cs", true)]
+        [InlineData("a,b,f.cs", "a,b,f.cs", false)]
+        [Theory]
+        public void DocumentIdNotEqualsOperator(string a, string b, bool expectedResult)
+        {
+            // arrange
+            DocumentId ida = DocumentIdFromString(a, ',');
+            DocumentId idb = DocumentIdFromString(b, ',');
+
+            // act
+            bool result = ida != idb;
+
+            // assert
+            result.Equals(expectedResult);
+        }
+
+        [Fact]
+        public void DocumentIdGetHashCode()
+        {
+            // arrange
+            DocumentId a1 = DocumentIdFromString("a,b,f.cs", ',');
+            DocumentId a2 = DocumentIdFromString("a,b,f.cs", ',');
+            DocumentId b = DocumentIdFromString("c,d,e.cs", ',');
+
+            // act
+            int hasha1 = a1.GetHashCode();
+            int hasha2 = a2.GetHashCode();
+            int hashb = b.GetHashCode();
+
+            // assert
+            hasha1.Should().Be(hasha2);
+            hasha1.Should().NotBe(hashb);
+        }
+
+
+        private DocumentId DocumentIdFromString(string s, char seperator)
+        {
+            string[] parts = s.Split(seperator);
+            return new DocumentId(parts.Last(), parts.Take(parts.Length - 1));
         }
     }
 }
--- src/Generator.ProjectSystem/DocumentId.cs
+++ src/Generator.ProjectSystem/DocumentId.cs
@@ -5,16 +5,29 @@ using Microsoft.CodeAnalysis;
 
 namespace ChilliCream.Logging.Generator
 {
-    public class DocumentId
+    /// <summary>
+    /// Represents a <see cref="Project"/> unique identifier of a <see cref="Document"/>.
+    /// </summary>
+    public sealed class DocumentId
     {
         private const string _separator = "/";
         private string _internalId;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocumentId"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="folders">The folders.</param>
         public DocumentId(string name, IEnumerable<string> folders)
         {
             _internalId = folders.Any() ? Path.Combine(Path.Combine(folders.ToArray()), name) : name;
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="DocumentId" /> is equal to this instance.
+        /// </summary>
+        /// <param name="other">The other <see cref="DocumentId" /> to compare with the current <see cref="DocumentId" />.</param>
+        /// <returns><c>true</c> if the specified <see cref="DocumentId" /> is equal to this instance; otherwise, <c>false</c>.</returns>
         public bool Equals(DocumentId other)
         {
             if (ReferenceEquals(other, null))
@@ -30,6 +43,11 @@ namespace ChilliCream.Logging.Generator
             return other._internalId.Equals(_internalId);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(obj, null))
@@ -39,6 +57,10 @@ namespace ChilliCream.Logging.Generator
             return Equals(obj as Document);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
         public override int GetHashCode()
         {
             unchecked
@@ -48,12 +70,49 @@ namespace ChilliCream.Logging.Generator
         }
 
         /// <summary>
-        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// Returns a <see cref="System.String" /> that represents the project relative path of the document.
         /// </summary>
-        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
+        /// <returns>A <see cref="System.String" /> that represents the project relative path of the document.</returns>
         public override string ToString()
         {
             return _internalId;
         }
+
+        #region Operators
+
+        /// <summary>
+        /// Implements the == operator.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>The result of the operator.</returns>
+        public static bool operator ==(DocumentId x, DocumentId y)
+        {
+            if (ReferenceEquals(x, null))
+            {
+                return !ReferenceEquals(y, null);
+            }
+            else
+            {
+                if (ReferenceEquals(y, null))
+                {
+                    return false;
+                }
+                return x.Equals(y);
+            }
+        }
+
+        /// <summary>
+        /// Implements the != operator.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>The result of the operator.</returns>
+        public static bool operator !=(DocumentId x, DocumentId y)
+        {
+            return !(x == y);
+        }
+
+        #endregion
     }
 }
