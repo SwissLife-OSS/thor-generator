25be5fd1e75678212eee82bfecbf01743a084f36
Michael Staib <michael@chillicream.com>
2017-09-22 21:51:32 +0200

    Added more bind task tests

--- src/Generator.Tests/BindTaskTests.cs
+++ src/Generator.Tests/BindTaskTests.cs
@@ -10,6 +10,21 @@ namespace ChilliCream.Tracing.Generator
     public class BindTaskTests
     {
         [Fact]
+        public void ConstructorArgumentValidation()
+        {
+            // arrange
+            List<TaskDefinition> taskDefinitions = new List<TaskDefinition>();
+
+            // act
+            Action a = () => new BindTask<CreateSolutionEventSources>(taskDefinitions);
+            Action b = () => new BindTask<CreateSolutionEventSources>(null);
+
+            // assert
+            a.ShouldNotThrow();
+            b.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
         public void AsDefault()
         {
             // arrange
@@ -40,6 +55,89 @@ namespace ChilliCream.Tracing.Generator
             taskDefinitions[0].IsDefault.Should().BeTrue();
             taskDefinitions[0].Name.Should().Be(name);
         }
+
+        [Fact]
+        public void AsDefaultDuplicate()
+        {
+            // arrange
+            List<TaskDefinition> taskDefinitions = new List<TaskDefinition>();
+            new BindTask<CreateSolutionEventSources>(taskDefinitions).AsDefault();
+            BindTask<CreateSolutionEventSources> bindTask = new BindTask<CreateSolutionEventSources>(taskDefinitions);
+
+            // act
+            Action a = () => bindTask.AsDefault();
+            Action b = () => bindTask.WithName("foo").AsDefault();
+
+            // assert
+            a.ShouldThrow<InvalidOperationException>();
+            b.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void WithName()
+        {
+            // arrange
+            List<TaskDefinition> taskDefinitions = new List<TaskDefinition>();
+            BindTask<CreateSolutionEventSources> bindTask = new BindTask<CreateSolutionEventSources>(taskDefinitions);
+            string name = Guid.NewGuid().ToString();
+
+            // act
+            bindTask.WithName(name);
+
+            // assert
+            taskDefinitions.Should().HaveCount(1);
+            taskDefinitions[0].Name.Should().Be(name);
+        }
+
+        [Fact]
+        public void WithNameAsDefault()
+        {
+            // arrange
+            List<TaskDefinition> taskDefinitions = new List<TaskDefinition>();
+            BindTask<CreateSolutionEventSources> bindTask = new BindTask<CreateSolutionEventSources>(taskDefinitions);
+            string name = Guid.NewGuid().ToString();
+
+            // act
+            bindTask.WithName(name).AsDefault();
+
+            // assert
+            taskDefinitions.Should().HaveCount(1);
+            taskDefinitions[0].IsDefault.Should().BeTrue();
+            taskDefinitions[0].Name.Should().Be(name);
+        }
+
+        [Fact]
+        public void WithNameArgumentValidation()
+        {
+            // arrange
+            List<TaskDefinition> taskDefinitions = new List<TaskDefinition>();
+            BindTask<CreateSolutionEventSources> bindTask = new BindTask<CreateSolutionEventSources>(taskDefinitions);
+
+            // act
+            Action a = () => bindTask.WithName(null);
+
+            // assert
+            a.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void WithNameDuplicateName()
+        {
+            // arrange
+            string name = Guid.NewGuid().ToString();
+            List<TaskDefinition> taskDefinitions = new List<TaskDefinition>();
+            new BindTask<CreateSolutionEventSources>(taskDefinitions).WithName(name);
+            BindTask<CreateSolutionEventSources> bindTask = new BindTask<CreateSolutionEventSources>(taskDefinitions);
+
+            // act
+            Action a = () => bindTask.WithName(name);
+            Action b = () => bindTask.AsDefault().WithName(name);
+
+            // assert
+            a.ShouldThrow<ArgumentException>();
+            b.ShouldThrow<ArgumentException>();
+        }
+
 
     }
 }
