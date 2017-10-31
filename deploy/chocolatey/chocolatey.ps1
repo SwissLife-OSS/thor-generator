c34b7a48d20bdf6a2d4c62701d0f9365059f52d6
Michael Staib <michael@chillicream.com>
2017-10-31 15:14:24 +0100

    Replace version in install script

--- deploy/chocolatey/chocolatey.ps1
+++ deploy/chocolatey/chocolatey.ps1
@@ -2,6 +2,11 @@ param([string]$Version, [string]$ApiKey)
 
 $nuspec = Join-Path -Path $PSScriptRoot -ChildPath "esgen.nuspec"
 $nupkg = Join-Path -Path $PSScriptRoot -ChildPath "esgen.$Version.nupkg"
+$installScript = Join-Path -Path $PSScriptRoot -ChildPath "tools/chocolatey.ps1"
+
+$installScriptContent = [System.IO.File]::ReadAllText($installScript)
+$installScriptContent = $installScriptContent.Replace("{version}", $Version)
+[System.IO.File]::WriteAllText($installScript, $installScriptContent)
 
 cpack $nuspec --version $Version --outdir $PSScriptRoot
 cpush $nupkg --apikey $ApiKey
